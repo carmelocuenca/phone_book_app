45c025370ca5cb50b3390084289194ace3c7f151
carmelo.cuenca <carmelocuenca@hotmail.com>
2014-04-05 14:50:12 +0100

    Cache with fragment expire working

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,18 @@ class Company < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   has_many :employees, dependent: :destroy
 
+  after_create   :expire_cache
+  # after_update   :expire_cache
+  # before_destroy :expire_cache
+
+  #after_touch    :expire_cache
+
   def to_s
     name
+  end
+
+  private
+  def expire_cache
+    ActionController::Base.new.expire_fragment(%r{table_of_all_companies/.*})
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,4 +8,5 @@ class Employee < ActiveRecord::Base
   def to_s
     "#{first_name} #{last_name}"
   end
+
 end
